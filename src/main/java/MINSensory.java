4555f20c51fa09077a27a5b87ffb8c44d72462c2
Ben Duong <bduong@vmware.com>
2012-01-09 19:06:45 -0500

    added Mac Application Icon

--- src/main/java/MINSensory.java
+++ src/main/java/MINSensory.java
@@ -1,4 +1,7 @@
+import com.apple.eawt.Application;
 import gui.UI;
+
+import javax.swing.*;
 
 /**
  * Created by IntelliJ IDEA. User: bduong Date: 1/2/12 Time: 6:43 PM To change
@@ -7,6 +10,7 @@ import gui.UI;
 public class MINSensory {
 
     public static void main(String [] args) {
+        ImageIcon imageIcon = new ImageIcon(UI.class.getResource("/MINSensory Logo.jpg"));
         UI gui = new UI();
         gui.init();
         gui.run();
