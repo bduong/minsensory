19072745b017e42e7c56f1bfe405fba250153e17
Ben Duong <benjaminf.duong@gmail.com>
2012-01-18 22:40:01 -0500

    first testing classes

---
+++ testing/src/main/java/data/generator/DataGen.java
@@ -0,0 +1,24 @@
+package data.generator;
+
+import data.DataLine;
+import java.util.Random;
+
+public class DataGen {
+
+    private Random random;
+    private int dataLineSize;
+
+    public DataGen(int dataLineSize) {
+        random = new Random(System.currentTimeMillis());
+        this.dataLineSize = dataLineSize;
+    }
+
+    public DataLine getNextLine() {
+        int [] colors = new int[dataLineSize];
+        for (int ii = 0; ii < dataLineSize; ii++){
+            colors[ii] = random.nextInt();
+        }
+
+        return new DataLine(colors);
+    }
+}
