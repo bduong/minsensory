de3a85a7f17dd74ba95d53b27989a10a68e04aa2
Ben Duong <benjaminf.duong@gmail.com>
2012-03-06 09:41:07 -0500

    Filled out ColorMap Enum.
    The RGB values need to be calculated.

--- mins/src/main/java/gui/ColorMap.java
+++ mins/src/main/java/gui/ColorMap.java
@@ -2,6 +2,22 @@ package gui
 
 public enum ColorMap {
 
+ALPHA(20),
+
+BETA(40),
+
+GAMMA(80),
+
+DELTA(160),
+
+THETA(320),
+
+SPIKE(640);
 
 
+public ColorMap(int color) {
+	rgb = color;
+}
+
+private int rgb;
 }
