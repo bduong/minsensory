69bd5668b132e352c4e81fd6612578f56381e5ef
Ben Duong <benjaminf.duong@gmail.com>
2012-05-25 12:37:59 -0400

    started frequency analysis stuff

---
+++ mins/src/main/java/freq/FrequencyAnalysisProcessor.java
@@ -0,0 +1,82 @@
+package freq;
+
+import data.playback.FileReader;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FrequencyAnalysisProcessor {
+
+    private List<List<Integer>> nodes;
+
+
+    public FrequencyAnalysisProcessor() {
+        nodes = new ArrayList<List<Integer>>(256);
+        for (int ii = 0; ii < 256; ii++){
+            nodes.add(new ArrayList<Integer>());
+        }
+
+
+    }
+
+    public void printStuff() {
+        for (List<Integer> node : nodes){
+            System.out.println(node.size());
+
+        }
+        System.out.println(nodes.size());
+        System.out.println("Done");
+    }
+
+    public void readData(File file) throws IOException {
+        for (List<Integer> node : nodes) {
+            node.clear();
+        }
+
+        FileReader fileReader = new FileReader(file);
+
+        outer:
+        while(true) {
+            for (List<Integer> node : nodes) {
+                try{
+                    int dataPoint = fileReader.readNextInt();
+                    node.add(dataPoint);
+                } catch (IOException e) {
+                    if (e.getMessage().equals("End of File")) {
+                        break outer;
+                    } else {
+                        throw e;
+                    }
+                }
+            }
+        }
+    }
+
+    public void runFrequencyAnalysis() {
+        for(List<Integer> node : nodes) {
+
+        }
+    }
+
+    public void saveData(File file) throws IOException {
+        BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(file));
+        int numberOfLines = nodes.get(0).size();
+        byte [] bytesToWrite = new byte[512];
+
+        for (int ii = 0; ii < numberOfLines; ii++) {
+            for (int jj = 0; jj < 256; jj++) {
+                int data = nodes.get(jj).get(ii);
+                bytesToWrite[jj*2] = (byte) (data >> 8);
+                bytesToWrite[jj*2 +1] = (byte) (data & 0x000000FF);
+            }
+
+            writer.write(bytesToWrite);
+
+        }
+
+    }
+}
--- testing/src/test/java/ui/UITests.java
+++ testing/src/test/java/ui/UITests.java
@@ -1,6 +1,7 @@
 package ui;
 
 
+import freq.FrequencyAnalysisProcessor;
 import gui.menu.items.ValidationTestPanel;
 import gui.tabs.FrequencyAnalysisPanel;
 import org.jfree.chart.ChartFactory;
@@ -16,80 +17,85 @@ import java.awt.*;
 public class UITests {
 
 
-    public static void main(String args []) {
-        JFrame frame = new JFrame("TabbedPaneDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    public static void main(String args []) {
+//        JFrame frame = new JFrame("TabbedPaneDemo");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        FrequencyAnalysisPanel panel = new FrequencyAnalysisPanel();
+//
+//        panel.setSaveFileSelected(new JLabel("File Name"));
+//        panel.setSelectSaveFileButton(new JButton("Select"));
+//        JFormattedTextField alpha =  new JFormattedTextField();
+//        alpha.setValue(50);
+//        alpha.setColumns(3);
+//        panel.setAlphaField(alpha);
+//
+//        JFormattedTextField beta =  new JFormattedTextField();
+//        beta.setValue(50);
+//        beta.setColumns(3);
+//        panel.setBetaField(beta);
+//
+//        JFormattedTextField delta =  new JFormattedTextField();
+//        delta.setValue(50);
+//        delta.setColumns(3);
+//        panel.setDeltaField(delta);
+//
+//        JFormattedTextField gamma =  new JFormattedTextField();
+//        gamma.setValue(50);
+//        gamma.setColumns(3);
+//        panel.setGammaField(gamma);
+//
+//        JFormattedTextField theta =  new JFormattedTextField();
+//        theta.setValue(50);
+//        theta.setColumns(3);
+//        panel.setThetaField(theta);
+//
+//
+//        panel.setAlphaSlider(new JSlider(0,100,50));
+//        panel.setBetaSlider(new JSlider(0,100,50));
+//        panel.setDeltaSlider(new JSlider(0,100,50));
+//        panel.setGammaSlider(new JSlider(0,100,50));
+//        panel.setThetaSlider(new JSlider(0,100,50));
+//
+//        panel.setProcessButton(new JButton("Process"));
+//
+//        Object [][] data = {
+//          {"Alpha", "8-12 Hz"},
+//          {"Beta", "13-30 Hz"},
+//          {"Delta", "1-4 Hz"},
+//          {"Gamma", "30-70 Hz"},
+//          {"Theta", "4-8 Hz"}
+//        };
+//
+//        panel.setBandColorTable(new JTable(data,new String[]{"Band", "Freq & Color"}));
+//
+//        panel.setFrequencyChart(new ChartPanel(ChartFactory.createXYLineChart(
+//          "Dummy",
+//          "Frequency",
+//          "Amp",
+//          new XYSeriesCollection(new XYSeries("Stuff")),
+//          PlotOrientation.VERTICAL,
+//          false,
+//          true,
+//          false
+//        )
+//        )
+//
+//        );
+//
+//         panel.layoutComponents();
+//
+//        //Add content to the window.
+////        frame.add(new SelectionPanel().getPlayOptions(), BorderLayout.CENTER);
+//        frame.add(panel, BorderLayout.CENTER);
+//        frame.setLocationRelativeTo(null);
+//        //Display the window.
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
 
-        FrequencyAnalysisPanel panel = new FrequencyAnalysisPanel();
-
-        panel.setSaveFileSelected(new JLabel("File Name"));
-        panel.setSelectSaveFileButton(new JButton("Select"));
-        JFormattedTextField alpha =  new JFormattedTextField();
-        alpha.setValue(50);
-        alpha.setColumns(3);
-        panel.setAlphaField(alpha);
-
-        JFormattedTextField beta =  new JFormattedTextField();
-        beta.setValue(50);
-        beta.setColumns(3);
-        panel.setBetaField(beta);
-
-        JFormattedTextField delta =  new JFormattedTextField();
-        delta.setValue(50);
-        delta.setColumns(3);
-        panel.setDeltaField(delta);
-
-        JFormattedTextField gamma =  new JFormattedTextField();
-        gamma.setValue(50);
-        gamma.setColumns(3);
-        panel.setGammaField(gamma);
-
-        JFormattedTextField theta =  new JFormattedTextField();
-        theta.setValue(50);
-        theta.setColumns(3);
-        panel.setThetaField(theta);
-
-
-        panel.setAlphaSlider(new JSlider(0,100,50));
-        panel.setBetaSlider(new JSlider(0,100,50));
-        panel.setDeltaSlider(new JSlider(0,100,50));
-        panel.setGammaSlider(new JSlider(0,100,50));
-        panel.setThetaSlider(new JSlider(0,100,50));
-
-        panel.setProcessButton(new JButton("Process"));
-
-        Object [][] data = {
-          {"Alpha", "8-12 Hz"},
-          {"Beta", "13-30 Hz"},
-          {"Delta", "1-4 Hz"},
-          {"Gamma", "30-70 Hz"},
-          {"Theta", "4-8 Hz"}
-        };
-
-        panel.setBandColorTable(new JTable(data,new String[]{"Band", "Freq & Color"}));
-
-        panel.setFrequencyChart(new ChartPanel(ChartFactory.createXYLineChart(
-          "Dummy",
-          "Frequency",
-          "Amp",
-          new XYSeriesCollection(new XYSeries("Stuff")),
-          PlotOrientation.VERTICAL,
-          false,
-          true,
-          false
-        )
-        )
-
-        );
-
-         panel.layoutComponents();
-
-        //Add content to the window.
-//        frame.add(new SelectionPanel().getPlayOptions(), BorderLayout.CENTER);
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setLocationRelativeTo(null);
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String [] args) {
+        FrequencyAnalysisProcessor freq = new FrequencyAnalysisProcessor();
+        freq.printStuff();
     }
 }
